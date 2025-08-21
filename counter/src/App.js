1f172e1b3b102854ca0d873e5cb92e1ddb42617c
Sarthak <sarthakss898@gmail.com>
2025-08-21 17:03:58 +0530

    Increment or decrement count by a custom number

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  function increment(){
-    setCount(count+1);
+  const [delta, setDelta] = useState(0);
+  function increment() {
+    setCount(count + 1);
   }
 
   return (
-      <Counter count={count} increment={increment}/>
+    <>
+      <Counter count={count} increment={increment} />
+      <h2>Custom count</h2>
+      <ChangeCount count={count} setCount={setCount} delta={delta} setDelta={setDelta}/>
+    </>
   );
 }
 
-function Counter({count, increment}){
-  return <div>
-    <h2>{count}</h2>
-    <button onClick={increment}>Click me!</button>
-  </div>
+function Counter({ count, increment }) {
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={increment}>Click me!</button>
+    </div>
+  );
+}
+
+function ChangeCount({ count, setCount, delta, setDelta }) {
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setDelta(value);
+  };
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setCount(
+            parseInt(count) + parseInt(delta)
+          );
+        }}
+      >
+        <label htmlFor="enteredcount">Change the count by</label>
+        <input type="text" id="enteredcount" onChange={handleChange} /><br />
+        <button type="submit">Increase</button>
+      </form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setCount(
+            parseInt(count) - parseInt(delta)
+          );
+        }}
+      >
+        <button type="submit">Decrease</button>
+      </form>
+    </>
+  );
 }
 
 export default App;

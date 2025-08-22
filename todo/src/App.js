2abce5e9d5f2babc3a51d4387634223aa73a4bd9
Sarthak <sarthakss898@gmail.com>
2025-08-22 20:40:34 +0530

    edit todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todolist, setTodolist] = useState([]);
+
+  const [editID, setEditID] = useState(null);
+  const [todolist, setTodolist] = useState(() => {
+    const savedlist = localStorage.getItem('toDoList');
+    return savedlist ? JSON.parse(savedlist) : []
+  });
+
+  useEffect(() => {
+    localStorage.setItem('toDoList', JSON.stringify(todolist));
+  }, [todolist]);
+
   return (
     <>
-      <Todos todolist={todolist} setTodolist={setTodolist}/>
-      <TodoForm todolist={todolist} setTodolist={setTodolist} />
+      <div>
+        <h1>Sarthak's todo list</h1>
+        <Todos todolist={todolist} setTodolist={setTodolist} setEditID={setEditID} editID={editID}/>
+        <EditForm editID={editID} setEditID={setEditID} todolist={todolist} setTodolist={setTodolist}/>
+      </div>
+      <AddForm todolist={todolist} setTodolist={setTodolist} />
     </>
   );
 }
 
-function Todos({todolist, setTodolist}) {
+function Todos({todolist, setTodolist, setEditID, editID}) {
   function removefromtodolist(index){
+    if(editID === index) setEditID(null);
     let newlist = [...todolist];
     newlist.splice(index, 1);
     setTodolist(newlist);
@@ -20,13 +35,13 @@ function Todos({todolist, setTodolist}) {
   return (
     <ol>
       {todolist.map((todo, index) => {
-        return <li key={index}>{todo} <button onClick={() => removefromtodolist(index)}>🗑️</button></li>;
+        return <li key={index}>{todo} <button onClick={()=>setEditID(index)}>✏️</button><button onClick={() =>  removefromtodolist(index)}>🗑️</button></li>;
       })}
     </ol>
   );
 }
 
-function TodoForm({todolist, setTodolist}) {
+function AddForm({todolist, setTodolist}) {
   const [task, setTask] = useState("");
   function addtask(e) {
     e.preventDefault();
@@ -48,6 +63,32 @@ function TodoForm({todolist, setTodolist}) {
         Add
       </button>
     </form>
+  );
+}
+
+function EditForm({todolist, setTodolist, editID, setEditID}) {
+  const [task, setTask] = useState("");
+  function editTask(e, newtask) {
+    e.preventDefault();
+    let newtodolist = [...todolist];
+    newtodolist[editID] = newtask;
+    setTodolist(newtodolist);
+    setEditID(null);
+  }
+  return (
+    editID !== null?<form onSubmit={e => task?editTask(e, task):e.preventDefault()}>
+      <label
+        htmlFor="task"
+      >
+        Edit task #{editID+1}:{" "}
+      </label>
+      <input type="text" id="task" onChange={(e) => {
+          setTask(e.target.value);
+        }}/>
+      <button type="submit">
+        Edit
+      </button>
+    </form>:null
   );
 }
 

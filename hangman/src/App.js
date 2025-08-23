a1f927f7a172e67f9d0d39d360914f4016345bca
Sarthak <sarthakss898@gmail.com>
2025-08-23 11:04:14 +0530

    More categories

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -13,7 +13,7 @@ function App() {
       const fetchnewword =  async () => {
         for(const option of selected){
           const response =  await fetch(
-            `/${option}.json`
+            `/content/${option}.json`
           );
           result.push( await response.json());   
         };
@@ -58,7 +58,7 @@ function Wordspace({ word, setChances, chances }) {
   const [currentChar, setCurrentChar] = useState("");
   const [currentWord, setCurrentWord] = useState(() => 
     toguess.map((char) => 
-      ['a','e','i','o','u',' ',':','-','.',"\"","'"].includes(char.toLowerCase()) ? char : '_'
+      ['a','e','i','o','u',' ',':','-','.','\"','\'','!',','].includes(char.toLowerCase()) ? char : '_'
     )
   );
 
@@ -111,12 +111,28 @@ function Options({selected, setSelected}){
     setSelected(newSelected);
   }
   return <>
-  <form>
-    <label htmlFor="movies">Movies</label>
-     <input type="checkbox" id="movies" value="movies" checked={selected.includes('movies')} onChange={(e) => handleChange(e)} />
-     <label htmlFor="tv-shows">TV Shows</label>
-     <input type="checkbox" id="tv-shows" value="tv-shows" checked={selected.includes('tv-shows')} onChange={(e) => handleChange(e)} />
-  </form>
+    <form>
+      <label htmlFor="movies">Movies</label>
+      <input type="checkbox" id="movies" value="movies" checked={selected.includes('movies')} onChange={(e) => handleChange(e)} />
+      
+      <label htmlFor="tv-shows">TV Shows</label>
+      <input type="checkbox" id="tv-shows" value="tv-shows" checked={selected.includes('tv-shows')} onChange={(e) => handleChange(e)} />
+      
+      <label htmlFor="songs">Songs</label>
+      <input type="checkbox" id="songs" value="songs" checked={selected.includes('songs')} onChange={(e) => handleChange(e)} />
+      
+      <label htmlFor="artists">Artists</label>
+      <input type="checkbox" id="artists" value="artists" checked={selected.includes('artists')} onChange={(e) => handleChange(e)} />
+      
+      <label htmlFor="video-games">Video Games</label>
+      <input type="checkbox" id="video-games" value="video-games" checked={selected.includes('video-games')} onChange={(e) => handleChange(e)} />
+      
+      <label htmlFor="books">Books</label>
+      <input type="checkbox" id="books" value="books" checked={selected.includes('books')} onChange={(e) => handleChange(e)} />
+    
+      <label htmlFor="bollywood">Bollywood</label>
+      <input type="checkbox" id="bollywood" value="bollywood" checked={selected.includes('bollywood')} onChange={(e) => handleChange(e)} />
+    </form>
   {error && "You need to have atleast one category selected!"}
   </>
 }

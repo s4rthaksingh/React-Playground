7aefca921619e808a16a091b89565e2579a7acf0
Sarthak <sarthakss898@gmail.com>
2025-08-26 22:21:59 +0530

    Select random color

--- color-guess-game/src/App.js
+++ color-guess-game/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('Null');
+
+  useEffect(() => {
+    const fetchcolor = async () => {
+    const response = await fetch('/colors.json');
+    const result = await response.json();
+    setColor(result[Math.floor(Math.random()*result.length)]);
+  }
+  fetchcolor();
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {color}
+    </>
   );
 }
 

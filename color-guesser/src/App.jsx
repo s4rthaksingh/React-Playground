0b4c7b624dc00b510450366ed9af2cc52ad7ecc2
Sarthak <sarthakss898@gmail.com>
2025-08-27 22:34:16 +0530

    Basic layout

--- color-guesser/src/App.jsx
+++ color-guesser/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-
+      <div className="flex flex-col items-center">
+        <div className="bg-amber-600 w-50 h-50 mb-30"></div>
+        <div className="flex flex-row gap-5">
+          <button className="w-50 h-20">A</button>
+          <button className="w-50 h-20">B</button>
+          <button className="w-50 h-20">C</button>
+          <button className="w-50 h-20">D</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

55dd7ee490d767fc782263d6b07da7c97a191cff
Sarthak <sarthakss898@gmail.com>
2025-08-27 23:29:30 +0530

    Select random number

--- color-guesser/src/App.jsx
+++ color-guesser/src/App.jsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [colorsObject, setColorsObject] = useState({});
+  const [color, setColor] = useState(0);
+
+  useEffect(() => {
+    const fetchColor = async () => {
+      const response = await fetch('/colors.json');
+      const result = await response.json();
+      setColorsObject(result);
+      setColor(Object.entries(result)[Math.floor(Math.random()*Object.entries(result).length)]);
+    }
+    fetchColor();
+  },[])
+
+
 
   return (
     <>
       <div className="flex flex-col items-center">
-        <div className="bg-amber-600 w-50 h-50 mb-30"></div>
+        <div className="w-50 h-50 mb-30" style={{background : color[1]}}></div>
         <div className="flex flex-row gap-5">
-          <button className="w-50 h-20">A</button>
-          <button className="w-50 h-20">B</button>
-          <button className="w-50 h-20">C</button>
-          <button className="w-50 h-20">D</button>
+          <button className="w-50 h-20">{color[0]}</button>
+          <button className="w-50 h-20">{color[0]}</button>
+          <button className="w-50 h-20">{color[0]}</button>
+          <button className="w-50 h-20">{color[0]}</button>
         </div>
       </div>
     </>

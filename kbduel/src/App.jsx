960653159a03d464645e9e4dbce2650fa3051632
Sarthak <sarthakss898@gmail.com>
2025-08-28 21:36:59 +0530

    Finish game

--- kbduel/src/App.jsx
+++ kbduel/src/App.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useRef } from "react";
 import "./App.css";
 
+
 function App() {
-  const [leftCount, setLeftCount] = useState(45);
+  const [leftCount, setLeftCount] = useState(0);
   const [rightCount, setRightCount] = useState(0);
-  const [gameRunning, setGameRunning] = useState(true);
+  const [gameRunning, setGameRunning] = useState(false);
   const [winner, setWinner] = useState(null);
+  const [score, setScore] = useState([0,0]);
+  const scoredRef = useRef(false);
+
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (!gameRunning || event.repeat) return;
+      if (event.repeat) return;
+      if (!gameRunning && event.code === "Enter") {
+        setGameRunning(true);
+        setWinner(null);
+        setLeftCount(0);
+        setRightCount(0);
+        scoredRef.current = false;
+        return;
+      }
       if (event.code === "KeyQ") {
         setLeftCount((prev) => {
           const next = prev + 1;
-          if (next >= 50) {
+          if (next === 50 && !scoredRef.current) {
+            scoredRef.current = true;
             setWinner("Player 1");
             setGameRunning(false);
+            setScore((prev) => {return [prev[0]+1, prev[1]]});
           }
           return next;
         });
@@ -25,9 +38,11 @@ function App() {
       if (event.code === "KeyL") {
         setRightCount((prev) => {
           const next = prev + 1;
-          if (next >= 50) {
-            setWinner("Player  1");
+          if (next === 50 && !scoredRef.current) {
+            scoredRef.current = true;
+            setWinner("Player 2");
             setGameRunning(false);
+            setScore((prev) => {return [prev[0], prev[1]+1]});
           }
           return next;
         });
@@ -41,26 +56,53 @@ function App() {
     };
   }, [gameRunning]);
 
-  return (
-    <>
-      <div className="flex min-w-screen min-h-screen gap-1">
-        <div className="bg-red-300 w-1/2">
-          <h1 className="text-black mt-6 font-thin flex items-center justify-center">
-            Player 1
-          </h1>
-          <div className="flex items-center justify-center h-full text-black  font-thin">
-            <h1 className="text-3xl mb-50">{leftCount}</h1>
-          </div>
-        </div>
-        <div className="bg-blue-300 w-1/2">
-          <h1 className="text-black mt-6 font-thin">Player 2</h1>
-          <div className="flex items-center justify-center h-full text-black  font-thin">
-            <h1 className="text-3xl mb-50">{rightCount}</h1>
-          </div>
+  
+  if (!gameRunning && winner)
+    return (
+      <div className="min-w-screen min-h-screen bg-amber-50 flex justify-center items-center pb-10">
+        <div className="flex flex-col">
+        <h1 className="text-black font-thin -mt-40 mb-30">{winner} wins!</h1>
+        <h1 className="text-black font-thin">
+          Press 'Enter' to start new game
+        </h1>
         </div>
       </div>
-    </>
-  );
+    );
+
+  if (!gameRunning && !winner)
+    return (
+      <div className="min-w-screen min-h-screen bg-amber-50 flex justify-center items-center pb-10">
+        <h1 className="text-black font-thin">
+          Press 'Enter' to start new game
+        </h1>
+      </div>
+    );
+
+
+  if (gameRunning && !winner)
+    return (
+      <>
+        <div className="flex min-w-screen min-h-screen gap-1 z-0">
+          <div className="bg-red-300 w-1/2">
+            <h1 className="text-black mt-6 font-thin flex items-center justify-center">
+              Player 1
+            </h1>
+            <div className="flex items-center justify-center h-full text-black  font-thin">
+              <h1 className="text-3xl mb-50">{leftCount}</h1>
+            </div>
+          </div>
+          <div className="bg-blue-300 w-1/2">
+            <h1 className="text-black mt-6 font-thin">Player 2</h1>
+            <div className="flex items-center justify-center h-full text-black  font-thin">
+              <h1 className="text-3xl mb-50">{rightCount}</h1>
+            </div>
+          </div>
+        </div>
+        <div className="fixed top-6  right-150 left-150  z-50 text-amber-50 font-mono px-3 py-1 rounded text-6xl">
+  <span>{score[0]} : {score[1]}</span>
+</div>
+      </>
+    );
 }
 
 export default App;

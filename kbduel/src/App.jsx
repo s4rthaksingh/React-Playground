4ddda31e9d4561e4ace1f90ebf4c779bb5482a74
Sarthak <sarthakss898@gmail.com>
2025-08-28 20:16:50 +0530

    Set up styling

--- kbduel/src/App.jsx
+++ kbduel/src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [leftCount, setLeftCount] = useState(0);
+  const [rightCount, setRightCount] = useState(0)
 
   return (
     <>
-      <div className='flex  w-screen h-screen'>
-        <div className='bg-green-300'>
-
+      <div className="flex min-w-screen min-h-screen gap-1">
+        <div className="bg-red-300 w-1/2">
+          <h1 className="text-black mt-6 font-thin flex items-center justify-center">Player 1</h1>
+            <div className="flex items-center justify-center h-full text-black  font-thin">
+              <h1 className="text-3xl mb-50">{leftCount}</h1>
+            </div>
         </div>
-        <div className='bg-red-400'>
-
+        <div className="bg-blue-300 w-1/2">
+          <h1 className="text-black mt-6 font-thin">Player 2</h1>
+            <div className="flex items-center justify-center h-full text-black  font-thin">
+              <h1 className="text-3xl mb-50">{rightCount}</h1>
+            </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

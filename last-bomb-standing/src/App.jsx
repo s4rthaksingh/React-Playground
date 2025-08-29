8c04669df04b86fba924f226a8b005667b7765b7
Sarthak <sarthakss898@gmail.com>
2025-08-29 22:34:36 +0530

    fml i cant do ts anymore

--- last-bomb-standing/src/App.jsx
+++ last-bomb-standing/src/App.jsx
@@ -1,31 +1,47 @@
 import { useState, useEffect } from 'react';
-import { ref, set, onValue } from 'firebase/database';
+import { ref, set, onValue, off, update } from 'firebase/database';
 import { db } from '../firebase';
 import './App.css';
 
 function App() {
-  const [currentPlayer, setCurrentPlayer] = useState(null);
+  const [playerIndex, setplayerIndex] = useState(null);
+  const [localPlayer, setLocalPlayer] = useState(0);
+  const [addedToPlayers, setAddedToPlayers] = useState(false);
 
-  useEffect(() => {
-    const playerRef = ref(db, 'game/currentPlayer');
-    const unsubscribe = onValue(playerRef, (snapshot) => {
+  useEffect(() => {  
+
+    const gameRef = ref(db, 'game');
+    const unsubscribe = onValue(gameRef, (snapshot) => {
       const data = snapshot.val();
-      console.log('Current Player:', data);
-      setCurrentPlayer(data);
-    });
-
-    return () => unsubscribe();
+      console.log('Current game state:', data);
+      setplayerIndex(data.playerIndex);
+      if(!data.players){
+        update(gameRef,{ players: [localPlayer] })
+        setAddedToPlayers(true);
+      }
+      if(data.players && data.players.includes(localPlayer) && !addedToPlayers){
+        setLocalPlayer(localPlayer+1);
+        update(gameRef, {players : []})
+        console.log("Local player : " + localPlayer);
+      }
+    })
+    return () => {
+      unsubscribe();
+    }
   }, []);
 
-  const handleClick = () => {
-    set(ref(db, 'game/currentPlayer'), { name: 'Alice' });
+  const handleClick = (player) => {
+    set(ref(db, 'game'), { playerIndex: player });
   };
 
   return (
-    <div>
-      <h1>Hot Bomb Game</h1>
-      <p>Current Player: {currentPlayer ? currentPlayer.name : 'Loading...'}</p>
-      <button onClick={handleClick}>Click to Set Player</button>
+    <div> 
+      {localPlayer === playerIndex && <h1>💣</h1>}
+      <p>Current Player: {playerIndex}</p>
+      <div className="flex gap-5">
+        <button onClick={() => handleClick(0)}>Give it to Player 1</button>
+        <button onClick={() => handleClick(1)}>Give it to Player 2</button>
+      </div>
     </div>
   );
 }

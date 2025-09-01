b5ecaa8ac2f89be23742007e5c912b597d9e4475
Sarthak <sarthakss898@gmail.com>
2025-09-01 10:21:02 +0530

    Add properties to the game and show them

--- real-estate-game/src/App.jsx
+++ real-estate-game/src/App.jsx
@@ -4,18 +4,24 @@ import "./App.css";
 function App() {
   const [bal, setBal] = useState(() => {
     const saved = localStorage.getItem('balance');
-    return saved ? parseFloat(saved) : 0.0;
+    return saved ? parseInt(saved) : 99;
   });
+  const [properties, setProperties] = useState(() => {
+    const savedProperties = localStorage.getItem("properties");
+    return savedProperties ? JSON.parse(savedProperties) : {};
+  })
 
   useEffect(() => {
     localStorage.setItem("balance", bal.toString());
-  },[bal])
+    localStorage.setItem("properties", JSON.stringify(properties))
+  },[bal, properties])
 
   return (
     <>
       <div className="h-screen w-screen flex items-center justify-around">
         <Workplace setBal={setBal} bal={bal} />
-        <Properties setBal={setBal} bal={bal} />
+        <BuyOption setBal={setBal} bal={bal} properties={properties} setProperties = {setProperties}/>
+        <Properties properties={properties}/>
       </div>
     </>
   );
@@ -38,14 +44,26 @@ function Workplace({ bal, setBal }) {
   );
 }
 
-function Properties({bal, setBal}){
-
+function BuyOption({bal, setBal, properties, setProperties}){
   const buyApartmentHandler = () => {
     setBal(bal-100);
+    let newProperties = {...properties, 'Apartment' : (properties["Apartment"] || 0) + 1};
+    setProperties(newProperties);
   }
 
   if(bal >= 100) return <>
     <button onClick={buyApartmentHandler}>Buy apartment ($100)</button>
+  </>
+}
+
+function Properties({properties}){
+  return <>
+    <div>
+      Your properties :
+      {Object.keys(properties).map((property, index) =>
+        {return <li key={index}>{property} : {properties[property]}</li>}
+      )}
+    </div>
   </>
 }
 

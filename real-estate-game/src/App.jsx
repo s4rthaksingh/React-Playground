d03eb30c52af1e3d466d99dc0756f3a5e355630e
Sarthak <sarthakss898@gmail.com>
2025-09-01 09:13:51 +0530

    Separate workspace component

--- real-estate-game/src/App.jsx
+++ real-estate-game/src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [bal, setBal] = useState(0)
+  const [bal, setBal] = useState(0);
 
   return (
     <>
-      <div className="h-screen w-screen flex flex-col justify-center items-center gap-3">
-        <h2>Your balance : {bal}</h2>
-        <button onClick={()=>{setBal(bal+1)}}>Work</button>
+      <div className="h-screen w-screen flex items-center justify-around">
+        <Workplace setBal={setBal} bal={bal} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+function Workplace({ bal, setBal }) {
+  return (
+    <>
+    <div className="flex flex-col gap-3">
+      <h2>Your balance : {bal}</h2>
+      <button
+        onClick={() => {
+          setBal(bal + 1);
+        }}
+      >
+        Work
+      </button>
+    </div>
+    </>
+  );
+}
+
+export default App;

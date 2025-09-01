77a169a87ca0177efd53d697141bbcb2d080737c
Sarthak <sarthakss898@gmail.com>
2025-09-01 09:28:21 +0530

    Save balance in local storage

--- real-estate-game/src/App.jsx
+++ real-estate-game/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [bal, setBal] = useState(0);
+  const [bal, setBal] = useState(() => {
+    const saved = localStorage.getItem('balance');
+    return saved ? parseFloat(saved) : 0.0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("balance", bal.toString());
+  },[bal])
 
   return (
     <>
       <div className="h-screen w-screen flex items-center justify-around">
         <Workplace setBal={setBal} bal={bal} />
+        <Properties setBal={setBal} bal={bal} />
       </div>
     </>
   );
@@ -28,6 +36,17 @@ function Workplace({ bal, setBal }) {
     </div>
     </>
   );
+}
+
+function Properties({bal, setBal}){
+
+  const buyApartmentHandler = () => {
+    setBal(bal-100);
+  }
+
+  if(bal >= 100) return <>
+    <button onClick={buyApartmentHandler}>Buy apartment ($100)</button>
+  </>
 }
 
 export default App;

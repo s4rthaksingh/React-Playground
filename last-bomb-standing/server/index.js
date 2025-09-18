931383e4aed044722c8c255e15534cea8b291042
Sarthak <sarthakss898@gmail.com>
2025-09-18 08:48:37 +0530

    Basic frontend and backend connection working!

--- last-bomb-standing/server/index.js
+++ last-bomb-standing/server/index.js
@@ -4,10 +4,23 @@ import http from "http";
 const server = http.createServer();
 const io = new Server(server, {cors : { origin : "*" }});
 
-const rooms = {};
+const gameState = {
+    players: [],
+    bombHolder: null,
+};
 
 io.on('connection', socket => {
-    console.log(socket.id + " connected!");
+    
+    gameState.players.push(socket.id);
+    console.log(socket.id + " joined!");
+    console.log(`Current players : ${gameState.players}`);
+    io.emit("state", gameState);
+
+    socket.on('disconnect', () => {
+        console.log(socket.id + " disconnected");
+        gameState.players = gameState.players.filter(p => p != socket.id);
+        io.emit("state", gameState);
+    })
 })
 
 server.listen(3000, () => console.log("Server running on port 3000"));
--- last-bomb-standing/src/App.jsx
+++ last-bomb-standing/src/App.jsx
@@ -2,5 +2,15 @@ import { useEffect, useState } from "react";
 import { socket } from "./socket";
 
 export default function App() {
-  return "Hello World!";
+  const [state, setState] = useState(null);
+  
+
+  useEffect(() => {
+    socket.on("state", setState);
+    return () => socket.off("state");
+  }, []);
+
+  return(
+    <p>{state && JSON.stringify(state)}</p>
+  );
 }

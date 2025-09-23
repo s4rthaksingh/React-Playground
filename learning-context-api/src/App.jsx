22e28c43833985b002cf5cfe0a4dd61bf302383d
Sarthak <sarthakss898@gmail.com>
2025-09-23 15:37:32 +0530

    Center a div

--- learning-context-api/src/App.jsx
+++ learning-context-api/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
 
   return (
-    <>
+    <div style={{display: "flex", justifyContent:"center", alignItems: "center", height: "100vh"}}>
       Hello World!
-    </>
+    </div>
   )
 }
 

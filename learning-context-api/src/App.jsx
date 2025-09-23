f015c15b182b41c9a4b5a8859d42542e4dc3fb47
Sarthak <sarthakss898@gmail.com>
2025-09-23 15:28:48 +0530

    Clear everything

--- learning-context-api/src/App.jsx
+++ learning-context-api/src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import { LevelContext } from './LevelContext'
-import Section from './Section'
-import Heading from './Heading'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Section level={1}>
-      <Heading children={"Hello World!"}></Heading>
-      <Heading children={"Hello World!"}></Heading>
-      <Heading children={"Hello World!"}></Heading>
-      <Heading children={"Hello World!"}></Heading>
-    </Section>
-   </>
+      Hello World!
+    </>
   )
 }
 

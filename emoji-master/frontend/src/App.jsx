267b78f9109c4d20c90d585b3fe41538cd641c5d
Sarthak <sarthakss898@gmail.com>
2025-10-01 17:03:46 +0530

    Lobby screen UI

--- emoji-master/frontend/src/App.jsx
+++ emoji-master/frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { io } from 'socket.io-client';
-import { useEffect } from 'react';
+import './App.css'
 
 const socket = io('http://localhost:3000')
 
 function App() {
+  const [roomID, setRoomID] = useState('');
+
   return (
     <>
-      <div className='h-screen w-screen items-center justify-center'>
-        <h1>Hello World!</h1>
+      <div className='h-full w-full flex flex-col items-center justify-center'>
+        <h1>Enter Room ID</h1> 
+        <input placeholder='Enter here...' className='m-3 border border-black rounded-sm px-3 py-2 focus:outline-none' type="text" onChange={(e) => setRoomID(e.target.value)}/>
+        <button className='hover:outline-2 bg-[#1a1a1a]'>Join</button>
       </div>
     </>
   )
